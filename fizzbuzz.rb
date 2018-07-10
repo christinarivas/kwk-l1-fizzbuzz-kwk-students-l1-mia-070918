3800d0df1ecb40fd69fdc1b0aa2317f5436116c0
christinarivas <christina@rivastwins.com>
2018-07-10 17:08:07 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(num)
+
+if num % 3 == 0 && num % 5 == 0
+  puts "Fizzbuzz" 
+  elsif num % 3 ==0 
+  puts "Fizz"
+elsif num % 5 == 0
+  puts "buzz"
+else
+  puts "nil"
+end
+  
+
+end
+
+fizzbuzz(15)
